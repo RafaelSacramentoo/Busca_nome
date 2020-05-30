03b284a9a5d02aafcca52a3722e76b9c9f8f338e
Rafael Sacramento <51892364+RafaelSacramentoo@users.noreply.github.com>
2020-05-30 19:15:38 -0300

    Add files via upload

---
+++ DESAFIO_BUSCA_LINEAR.py
@@ -0,0 +1,13 @@
+
+
+pessoa = ["Namibia", "Carlos", "Ana", "Roberto", "Maria", "Eduardo", "Rafael", "Melissa", "Carol", "Beatriz", "Ana"]
+
+pessoa_1 = "Naruto" 
+
+print("Namibia", "Carlos", "Ana", "Roberto", "Maria", "Eduardo", "Rafael", "Melissa", "Carol", "Beatriz", "Ana")
+pessoa_pesquisada = str(input("Digite a pessoa a ser pesquisada:"))
+if pessoa.append(pessoa_1) == pessoa_pesquisada:
+    print(pessoa_1)
+else:
+    print(pessoa[-1])
+    
